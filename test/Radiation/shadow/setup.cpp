0fc420aa99b0e144ad44d87f44702e1dc75a8902
nscepi <nicolas.scepi@gmail.com>
2024-09-30 16:10:39 +0200

    Updated shadow test problem and added testme.py

--- test/Radiation/shadow/setup.cpp
+++ test/Radiation/shadow/setup.cpp
@@ -1,20 +1,25 @@
 #include "idefix.hpp"
 #include "setup.hpp"
 
-real unit_velocity;
-real unit_length;
-real unit_density;
-real rho_0;
-real T_0;
+real rho0Glob;
+real rho1Glob;
+real T0Glob;
+real TinjGlob;
+real muGlob;
+real x02Glob;
+real y02Glob;
 
 
 void UserdefBoundaryRad(Fluid<RadiationPhysics> *radiation, int dir, BoundarySide side, real t) {
   IdefixArray4D<real> Vc = radiation->Vc;
   auto *data = radiation->data;
 
-  real C_c = 29979245800.0;
-  real C_ar = 7.5646e-15;
-  real T = 1740.;
+  real unit_velocity = idfx::units.velocity;
+  real unit_density = idfx::units.density;
+  real unit_energy = unit_density*unit_velocity*unit_velocity;
+
+  real C_ar = idfx::units.ar;
+  real Tinj = TinjGlob;
 
   IdefixArray1D<real> x1 = data->x[IDIR];
   IdefixArray1D<real> x2 = data->x[JDIR];
@@ -29,7 +34,7 @@ void UserdefBoundaryRad(Fluid<RadiationPhysics> *radiation, int dir, BoundarySid
         0, data->np_tot[JDIR],
         ibeg, iend,
         KOKKOS_LAMBDA (int k, int j, int i) {
-          Vc(ER,k,j,i) = C_ar*std::pow(T,4);
+          Vc(ER,k,j,i) = C_ar*std::pow(Tinj,4)/unit_energy;
           Vc(FR1,k,j,i) = Vc(ER,k,j,i);
           Vc(FR2,k,j,i) = 0.;
         });
@@ -62,30 +67,6 @@ void UserdefBoundary(Fluid<DefaultPhysics> *hydro, int dir, BoundarySide side, r
   }
 }
 
-// Default constructor
-// Initialisation routine. Can be used to allocate
-// Arrays or variables which are used later on
-Setup::Setup(Input &input, Grid &grid, DataBlock &data, Output &output)
-{
-
-  unit_velocity = input.Get<real>("Units","velocity",0);
-  unit_length = input.Get<real>("Units","length",0);
-  unit_density = input.Get<real>("Units","density",0);
-  rho_0 = input.Get<real>("Rad","kappa",2);
-  T_0 = input.Get<real>("Rad","kappa",3);
-
-  //output.EnrollUserDefVariables(&ComputeUserVars);
-  // Set the function for userdefboundary
-  if(data.haveRadiation) {
-    int nFrequencies = data.radiation.size();
-    for(int n = 0 ; n < nFrequencies ; n++) {
-      data.radiation[n]->EnrollUserDefBoundary(&UserdefBoundaryRad);
-    }
-    data.hydro->EnrollUserDefBoundary(&UserdefBoundary);
-    //data.hydro->EnrollInternalBoundary(&InternalBoundary);
-
-  }
-}
 
 // This routine initialize the flow
 // Note that data is on the device.
@@ -94,28 +75,31 @@ Setup::Setup(Input &input, Grid &grid, DataBlock &data, Output &output)
 void Setup::InitFlow(DataBlock &data) {
     // Create a host copy
     DataBlockHost d(data);
-    real C_kb = 1.38e-16;
-    real C_mp = 1.6726e-24;
-    real C_ar = 7.5646e-15;
-    real mu = 1.;
-    real KELVIN = C_kb/(C_mp*mu);
-    real x02 = 0.01/(unit_length*unit_length);
-    real y02 = 0.0036/(unit_length*unit_length);
-    real rho_1 = 1.e3;
+    
+    real unit_length = idfx::units.length;
+    real unit_velocity = idfx::units.velocity;
+    real unit_density = idfx::units.density;
 
+    real C_ar = idfx::units.ar;
+    real mu = muGlob;
+    real KELVIN = idfx::units.Kelvin*mu;
+    real rho0 = rho0Glob; 
+    real rho1 = rho1Glob; 
+    real T0 = T0Glob;
+    real x02 = x02Glob/(unit_length*unit_length);
+    real y02 = y02Glob/(unit_length*unit_length);
 
-    real unit_time = unit_length/unit_velocity;
     real unit_energy = unit_density*unit_velocity*unit_velocity;
 
     for(int k = 0; k < d.np_tot[KDIR] ; k++) {
         for(int j = 0; j < d.np_tot[JDIR] ; j++) {
             for(int i = 0; i < d.np_tot[IDIR] ; i++) {
               real delta = 10.*(d.x[IDIR](i)*d.x[IDIR](i)/x02+d.x[JDIR](j)*d.x[JDIR](j)/y02-1.);
-              d.Vc(RHO,k,j,i) = rho_0+(rho_1-rho_0)/(1.+std::exp(delta));
-              d.Vc(PRS,k,j,i) = d.Vc(RHO,k,j,i)*unit_density*T_0*KELVIN/(unit_energy);
+              d.Vc(RHO,k,j,i) = rho0+(rho1-rho0)/(1.+std::exp(delta));
+              d.Vc(PRS,k,j,i) = d.Vc(RHO,k,j,i)*T0/KELVIN;
               d.Vc(VX1,k,j,i) = 0.;
               d.Vc(VX2,k,j,i) = 0.;
-              d.RadVc[0](ER,k,j,i) = 4.*C_kb*std::pow(T_0,4)/unit_energy;
+              d.RadVc[0](ER,k,j,i) = C_ar*std::pow(T0,4)/unit_energy;
               d.RadVc[0](FR1,k,j,i) = ZERO_F;
               d.RadVc[0](FR2,k,j,i) = ZERO_F;
             }
@@ -146,6 +130,33 @@ void ComputeUserVars(DataBlock & data, UserDefVariablesContainer &variables) {
     }
   }
 }
+
+// Default constructor
+// Initialisation routine. Can be used to allocate
+// Arrays or variables which are used later on
+Setup::Setup(Input &input, Grid &grid, DataBlock &data, Output &output)
+{
+  muGlob = input.Get<real>("Hydro","mu",0);
+  rho0Glob = input.Get<real>("Rad","kappa",2);
+  T0Glob = input.Get<real>("Rad","kappa",3);
+  rho1Glob = input.Get<real>("Setup","rho1",0);
+  TinjGlob = input.Get<real>("Setup","Tinj",0);
+  x02Glob = input.Get<real>("Setup","x02",0);
+  y02Glob = input.Get<real>("Setup","y02",0);
+
+  //output.EnrollUserDefVariables(&ComputeUserVars);
+  // Set the function for userdefboundary
+  if(data.haveRadiation) {
+    int nFrequencies = data.radiation.size();
+    for(int n = 0 ; n < nFrequencies ; n++) {
+      data.radiation[n]->EnrollUserDefBoundary(&UserdefBoundaryRad);
+    }
+    data.hydro->EnrollUserDefBoundary(&UserdefBoundary);
+    //data.hydro->EnrollInternalBoundary(&InternalBoundary);
+
+  }
+}
+
 
 
 
