5af188aeef2188b991866996c12029451603b8a9
nscepi <nicolas.scepi@gmail.com>
2024-10-03 09:25:34 +0200

    Updated Diffusion_disk test problem and added testme.py

---
+++ test/Radiation/Diffusion_Disk/testme.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+"""
+
+@author: glesur
+"""
+import os
+import sys
+sys.path.append(os.getenv("IDEFIX_DIR"))
+
+import pytools.idfx_test as tst
+
+name="dump.0001.dmp"
+
+tolerance=1e-13
+
+def testMe(test):
+  test.configure()
+  test.compile()
+  inifiles=["idefix-hll-c01.ini","idefix-hll-c001.ini","idefix-hll-c0001.ini","idefix-hllc-c01.ini","idefix-hllc-c001.ini","idefix-hllc-c0001.ini"]
+
+  # loop on all the ini files for this test
+  for ini in inifiles:
+    test.run(inputFile=ini)
+    if test.init and not test.mpi:
+      test.makeReference(filename=name)
+    test.standardTest()
+    test.nonRegressionTest(filename=name,tolerance=tolerance)
+
+
+test=tst.idfxTest()
+if not test.dec:
+  test.dec=['2','2']
+
+if not test.all:
+  if(test.check):
+    test.checkOnly(filename=name,tolerance=tolerance)
+  else:
+    testMe(test)
+else:
+  for rec in range(1,4):
+    test.vectPot=False
+    test.single=False
+    test.reconstruction=rec
+    test.mpi=False
+    testMe(test)
+
+  test.mpi=True
+  testMe(test)
