edf8d8e0bc0a6be80e6a12de8c861bd95e76078c
nscepi <nicolas.scepi@gmail.com>
2024-11-06 15:56:52 +0100

    Updated free_streaming_beam test with all parameters in idefix.ini

--- test/Radiation/free_streaming_beam/setup.cpp
+++ test/Radiation/free_streaming_beam/setup.cpp
@@ -1,6 +1,57 @@
 #include "idefix.hpp"
 #include "setup.hpp"
 
+real csisoGlob;
+real ER0Glob,ERbeamGlob,RhoGlob,AngleGlob,x1begGlob,x1endGlob,x2begGlob,x2endGlob;
+
+void InternalBoundaryRad(Fluid<RadiationPhysics> *radiation, const real t) {
+  IdefixArray4D<real> Vc = radiation->Vc;
+  auto *data = radiation->data;
+  IdefixArray1D<real> x1 = data->x[IDIR];
+  IdefixArray1D<real> x2 = data->x[JDIR];
+  int iend;
+  real x1beg = x1begGlob;
+  real x1end = x1endGlob;
+  real x2beg = x2begGlob;
+  real x2end = x2endGlob;
+  real ERbeam = ERbeamGlob;
+  real Angle  = AngleGlob;
+
+  iend = data->beg[IDIR];
+  idefix_for("InternalBoundaryFuncRad",
+    0, data->np_tot[KDIR],
+    0, data->np_tot[JDIR],
+    iend, data->np_tot[IDIR],
+    KOKKOS_LAMBDA (int k, int j, int i) {
+      if ((x2(j) > x2beg) && (x2(j) < x2end) && (x1(i) > x1beg) && (x1(i) < x1end)) {
+            Vc(ER,k,j,i) = ERbeam;;
+            Vc(FR1,k,j,i) = ERbeam*std::cos(Angle);
+            Vc(FR2,k,j,i) = ERbeam*std::sin(Angle);
+      }
+    });
+
+}
+
+Setup::Setup(Input &input, Grid &grid, DataBlock &data, Output &output)
+{
+  csisoGlob = input.Get<real>("Hydro","csiso",1);
+  ER0Glob = input.Get<real>("Setup","ER0",0);
+  ERbeamGlob = input.Get<real>("Setup","ERbeam",0);
+  AngleGlob = input.Get<real>("Setup","Angle",0);
+  RhoGlob = input.Get<real>("Setup","RHO",0);
+  x1begGlob = input.Get<real>("Setup","x1beg",0);
+  x1endGlob = input.Get<real>("Setup","x1end",0);
+  x2begGlob = input.Get<real>("Setup","x2beg",0);
+  x2endGlob = input.Get<real>("Setup","x2end",0);
+
+  if(data.haveRadiation) {
+    int nFrequencies = data.radiation.size();
+    for(int n = 0 ; n < nFrequencies ; n++) {
+      data.radiation[n]->EnrollInternalBoundary(&InternalBoundaryRad);
+    }
+
+  }
+}
 
 // This routine initialize the flow
 // Note that data is on the device.
@@ -9,19 +60,20 @@
 void Setup::InitFlow(DataBlock &data) {
     // Create a host copy
     DataBlockHost d(data);
-    real csiso = 0.1;
+    real csiso = csisoGlob;
+    real rho0 = RhoGlob;
 
 
     for(int k = 0; k < d.np_tot[KDIR] ; k++) {
         for(int j = 0; j < d.np_tot[JDIR] ; j++) {
             for(int i = 0; i < d.np_tot[IDIR] ; i++) {
 
-              d.Vc(RHO,k,j,i) = 1.e4;
-              d.Vc(VX1,k,j,i) = 0.;
-              d.Vc(VX2,k,j,i) = 0.;
+              d.Vc(RHO,k,j,i) = rho0;
+              d.Vc(VX1,k,j,i) = ZERO_F;
+              d.Vc(VX2,k,j,i) = ZERO_F;
               d.Vc(PRS,k,j,i) = d.Vc(RHO,k,j,i)*csiso*csiso;
 
-              d.RadVc[0](ER,k,j,i) = 1.e4;
+              d.RadVc[0](ER,k,j,i) = ER0Glob;
               d.RadVc[0](FR1,k,j,i) = ZERO_F;
               d.RadVc[0](FR2,k,j,i) = ZERO_F;
               
@@ -52,49 +104,6 @@ void ComputeUserVars(DataBlock & data, UserDefVariablesContainer &variables) {
 
       }
     }
-  }
-}
-
-void InternalBoundaryRad(Fluid<RadiationPhysics> *radiation, const real t) {
-  IdefixArray4D<real> Vc = radiation->Vc;
-  auto *data = radiation->data;
-  IdefixArray1D<real> x1 = data->x[IDIR];
-  IdefixArray1D<real> x2 = data->x[JDIR];
-  int ibeg,iend,nxi,iref;
-
-  ibeg = 0;
-  iend = data->beg[IDIR];
-  nxi = data->np_int[IDIR];
-  iref = iend;
-  idefix_for("InternalBoundaryFuncRad",
-    0, data->np_tot[KDIR],
-    0, data->np_tot[JDIR],
-    iend, data->np_tot[IDIR],
-    KOKKOS_LAMBDA (int k, int j, int i) {
-      //if ((j > 12) && (j < 14) && (i > 0) && (i < 4)) {
-      if ((x2(j) > 0.3) && (x2(j) < 0.44) && (x1(i) > 0.5) && (x1(i) < 0.6)) {
-            Vc(ER,k,j,i) = 1.e12;
-            Vc(FR1,k,j,i) = 1.e12*std::cos(M_PI/4.);
-            Vc(FR2,k,j,i) = 1.e12*std::sin(M_PI/4.);
-      }
-    });
-  
-}
-
-
-
-Setup::Setup(Input &input, Grid &grid, DataBlock &data, Output &output)
-{
-  //output.EnrollUserDefVariables(&ComputeUserVars);
-  // Set the function for userdefboundary
-  if(data.haveRadiation) {
-    int nFrequencies = data.radiation.size();
-    for(int n = 0 ; n < nFrequencies ; n++) {
-      //data.radiation[n]->EnrollUserDefBoundary(&UserdefBoundaryRad);
-      data.radiation[n]->EnrollInternalBoundary(&InternalBoundaryRad);
-    }
-    //data.hydro->EnrollUserDefBoundary(&UserdefBoundary);
-
   }
 }
 
