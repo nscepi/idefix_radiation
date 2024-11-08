c4961ebb7f22b75dbbea0f4f975c7520c5b439d1
nscepi <nicolas.scepi@gmail.com>
2024-11-08 11:45:05 +0100

    Updated shock_sub_critical reference test

--- test/Radiation/shock_sub_critical/setup.cpp
+++ test/Radiation/shock_sub_critical/setup.cpp
@@ -1,79 +1,19 @@
 #include "idefix.hpp"
 #include "setup.hpp"
 
-real unit_velocity;
-real unit_length;
-real unit_density;
-
-
-void UserdefBoundaryRad(Fluid<RadiationPhysics> *radiation, int dir, BoundarySide side, real t) {
-  IdefixArray4D<real> Vc = radiation->Vc;
-  auto *data = radiation->data;
-  IdefixArray1D<real> x1 = data->x[IDIR];
-  IdefixArray1D<real> x2 = data->x[JDIR];
-  if(dir==IDIR) {
-    int ighost,ibeg,iend;
-    if(side == left) {
-      ighost = data->beg[IDIR];
-      ibeg = 0;
-      iend = data->beg[IDIR];
-      idefix_for("UserDefBoundaryRad",
-        0, data->np_tot[KDIR],
-        0, data->np_tot[JDIR],
-        ibeg, iend,
-        KOKKOS_LAMBDA (int k, int j, int i) {
-          Vc(ER,k,j,i) = Vc(ER,k,j,2*ighost-i-1);
-          Vc(FR1,k,j,i) = Vc(FR1,k,j,ighost);
-        });
-    }
-  }
-}
-
-void UserdefBoundary(Fluid<DefaultPhysics> *hydro, int dir, BoundarySide side, real t) {
-  IdefixArray4D<real> Vc = hydro->Vc;
-  auto *data = hydro->data;
-  IdefixArray1D<real> x1 = data->x[IDIR];
-  IdefixArray1D<real> x2 = data->x[JDIR];
-  if(dir==IDIR) {
-    int ighost,ibeg,iend;
-    if(side == left) {
-      ighost = data->beg[IDIR];
-      ibeg = 0;
-      iend = data->beg[IDIR];
-      idefix_for("UserDefBoundary",
-        0, data->np_tot[KDIR],
-        0, data->np_tot[JDIR],
-        ibeg, iend,
-        KOKKOS_LAMBDA (int k, int j, int i) {
-          Vc(RHO,k,j,i) = Vc(RHO,k,j,2*ighost-i-1);
-          Vc(PRS,k,j,i) = Vc(PRS,k,j,2*ighost-i-1);
-          Vc(VX1,k,j,i) = -Vc(VX1,k,j,2*ighost-i-1);
-        });
-    }
-  }
-}
+real rho0Glob,vx1Glob,T0Glob,muGlob;
 
 // Default constructor
 // Initialisation routine. Can be used to allocate
 // Arrays or variables which are used later on
 Setup::Setup(Input &input, Grid &grid, DataBlock &data, Output &output)
 {
+  rho0Glob = input.Get<real>("Setup","rho0",0);
+  vx1Glob = input.Get<real>("Setup","vx1",0);
+  T0Glob = input.Get<real>("Setup","T0",0);
+  muGlob = input.Get<real>("Hydro","mu",0);
 
-  unit_velocity = input.Get<real>("Units","velocity",0);
-  unit_length = input.Get<real>("Units","length",0);
-  unit_density = input.Get<real>("Units","density",0);
 
-  //output.EnrollUserDefVariables(&ComputeUserVars);
-  // Set the function for userdefboundary
-  if(data.haveRadiation) {
-    int nFrequencies = data.radiation.size();
-    for(int n = 0 ; n < nFrequencies ; n++) {
-      //data.radiation[n]->EnrollUserDefBoundary(&UserdefBoundaryRad);
-    }
-    //data.hydro->EnrollUserDefBoundary(&UserdefBoundary);
-    //data.hydro->EnrollInternalBoundary(&InternalBoundary);
-
-  }
 }
 
 // This routine initialize the flow
@@ -83,28 +23,25 @@ Setup::Setup(Input &input, Grid &grid, DataBlock &data, Output &output)
 void Setup::InitFlow(DataBlock &data) {
     // Create a host copy
     DataBlockHost d(data);
-    real C_kb = 1.38e-16;
-    real C_mp = 1.6726e-24;
-    real C_ar = 7.5646e-15;
-    real mu = 1.;
-    real KELVIN = C_kb/(C_mp*mu);
-    real T = 10.;
-    //real T = 1.e9;
+    real C_kb = idfx::units.k_B;
+    real C_amu = idfx::units.u;
+    real C_ar = idfx::units.ar;
+    real T = T0Glob;
+    real rho0 = rho0Glob;
+    real vx1 = vx1Glob;
+    real mu = muGlob;
 
-    real unit_time = unit_length/unit_velocity;
+    real unit_density = idfx::units.density;
+    real unit_velocity = idfx::units.velocity;
     real unit_energy = unit_density*unit_velocity*unit_velocity;
     
     for(int k = 0; k < d.np_tot[KDIR] ; k++) {
         for(int j = 0; j < d.np_tot[JDIR] ; j++) {
             for(int i = 0; i < d.np_tot[IDIR] ; i++) {
-              d.Vc(RHO,k,j,i) = 7.78e-10/unit_density;
-              //d.Vc(RHO,k,j,i) = 1.e-7/unit_density;
-              d.Vc(PRS,k,j,i) = d.Vc(RHO,k,j,i)*unit_density*KELVIN*T/(unit_energy);
-              //d.Vc(PRS,k,j,i) = d.Vc(RHO,k,j,i)*unit_density*KELVIN*T/(unit_energy);
-              d.Vc(VX1,k,j,i) = -6.0e5/unit_velocity;
-              //d.Vc(VX1,k,j,i) = ZERO_F;
+              d.Vc(RHO,k,j,i) = rho0/unit_density;
+              d.Vc(PRS,k,j,i) = d.Vc(RHO,k,j,i)*unit_density*C_kb*T/(mu*C_amu*unit_energy);
+              d.Vc(VX1,k,j,i) = vx1/unit_velocity;
               d.RadVc[0](ER,k,j,i) = C_ar*std::pow(T,4)/(unit_energy);
-              //d.RadVc[0](ER,k,j,i) = 1.e4/unit_energy;
               d.RadVc[0](FR1,k,j,i) = ZERO_F;
             }
         }
