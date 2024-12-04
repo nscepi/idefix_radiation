7b06d6696383b77cc384feeab24f189a79d6417c
nscepi <nicolas.scepi@gmail.com>
2024-12-04 17:12:37 +0100

    Change tolerance according to MPI or not because of computation of column accross MPI domains

---
+++ test/Radiation/stellar_irradiation/testme.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+"""
+
+@author: glesur
+"""
+import os
+import sys
+sys.path.append(os.getenv("IDEFIX_DIR"))
+
+import pytools.idfx_test as tst
+import shutil
+
+name="dump.0001.dmp"
+
+def testMe(test,mpi):
+  inifiles=["idefix_constant_kappa.ini","idefix_usertable_kappa.ini"]
+  if mpi==True:
+     tolerance = 1.e-10  
+  else:
+     tolerance = 1.e-13
+
+  # loop on all the ini files for this test
+  for ini in inifiles:
+    test.configure()
+    test.compile()
+    test.run(inputFile=ini)
+    if test.init and not test.mpi:
+      test.makeReference(filename=name)
+    test.standardTest()
+    test.nonRegressionTest(filename=name,tolerance=tolerance)
+
+
+test=tst.idfxTest()
+test._readLog()
+
+if not test.dec:
+  test.dec=['2','2']
+
+if not test.all:
+  if(test.check):
+    if test.mpi: 
+     tolerance = 1.e-10
+    else:
+     tolerance = 1.e-13
+    test.checkOnly(filename=name,tolerance=tolerance)
+  else:
+    testMe(test,test.mpi)
+else:
+  for rec in range(1,4):
+    test.vectPot=False
+    test.single=False
+    test.reconstruction=rec
+    test.mpi=False
+    testMe(test,test.mpi)
+
+  test.mpi=True
+  testMe(test,test.mpi)
