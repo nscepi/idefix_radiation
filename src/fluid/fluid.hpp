7a41c1b70e0b7d861660db86ed0d6a8dd6fe3563
nscepi <nicolas.scepi@gmail.com>
2024-12-16 15:49:30 +0100

    Moved prefix initialization on top of constructor

--- src/fluid/fluid.hpp
+++ src/fluid/fluid.hpp
@@ -270,12 +270,12 @@ class Fluid {
 
 template<typename Phys>
 Fluid<Phys>::Fluid(Grid &grid, Input &input, DataBlock *datain, int n) {
+  // Create our own prefix
+  prefix = std::string(Phys::prefix);
+
   idfx::pushRegion("Fluid_"+prefix+"::Fluid");
   // Save the datablock to which we are attached from now on
   this->data = datain;
-
-  // Create our own prefix
-  prefix = std::string(Phys::prefix);
 
   // When dealing with dust, add the specie number
   if(Phys::prefix.compare("Dust") == 0) prefix += std::to_string(n);
