8a25d5be316415f47e86daeebf55c79f3f2098c5
nscepi <nicolas.scepi@gmail.com>
2026-02-11 17:27:22 +0100

    Fix units in radiation_drag test

--- test/Radiation/radiation_drag/setup.cpp
+++ test/Radiation/radiation_drag/setup.cpp
@@ -14,17 +14,15 @@ void FluxBoundary(Fluid<DefaultPhysics> *hydro, int dir, BoundarySide side, cons
 
     auto *data = hydro->data;
 
-    IdefixArray4D<real> Flux = data->hydro->FluxRiemann;
-    
-    if( dir== IDIR ) {
-      idefix_for("FluxInternal",
-                  0, data->np_tot[KDIR],
-                  0, data->np_tot[JDIR],
-                  0, data->np_tot[IDIR],
-         KOKKOS_LAMBDA (int k, int j, int i) {
-           Flux(RHO, k, j, i) = 0.0; 
-      });
-    }
+    IdefixArray4D<real> FluxIDIR = data->hydro->FluxRiemann[IDIR];
+
+    idefix_for("FluxInternal",
+                0, data->np_tot[KDIR],
+                0, data->np_tot[JDIR],
+                0, data->np_tot[IDIR],
+       KOKKOS_LAMBDA (int k, int j, int i) {
+         FluxIDIR(RHO, k, j, i) = 0.0;
+    });
     idfx::popRegion();
 
 }
@@ -35,17 +33,16 @@ void FluxBoundaryRad(Fluid<RadiationPhysics> *radiation, int dir, BoundarySide s
 
     auto *data = radiation->data;
 
-    IdefixArray4D<real> Flux = data->radiation[0]->FluxRiemann;
-    
-    if( dir== IDIR ) {
-      idefix_for("FluxInternal",
-                  0, data->np_tot[KDIR],
-                  0, data->np_tot[JDIR],
-                  0, data->np_tot[IDIR],
-         KOKKOS_LAMBDA (int k, int j, int i) {
-           Flux(ER, k, j, i) = 0.0; 
-      });
-    }
+    IdefixArray4D<real> FluxIDIR = data->radiation[0]->FluxRiemann[IDIR];
+
+    idefix_for("FluxInternal",
+               0, data->np_tot[KDIR],
+               0, data->np_tot[JDIR],
+               0, data->np_tot[IDIR],
+      KOKKOS_LAMBDA (int k, int j, int i) {
+        FluxIDIR(ER, k, j, i) = 0.0;
+    });
+
     idfx::popRegion();
 
 }
@@ -55,14 +52,12 @@ void InternalBoundary(Fluid<DefaultPhysics> *hydro, const real t) {
   auto *data = hydro->data;
 
   real mu = muGlob;
-  real cs = idfx::units.c/BigCGlob;
-  real T = cs*cs*mu*idfx::units.u/idfx::units.k_B;
-  real P = idfx::units.ar*std::pow(T,4.)/BigPGlob;
-  real rho = P/(cs*cs);
+  real rho = idfx::units.ar/BigPGlob*std::pow(mu*idfx::units.u/idfx::units.k_B,4.)*std::pow(idfx::units.c/BigCGlob,6.)/idfx::units.GetDensity();
 
   idefix_for("InternalBoundary",0,data->np_tot[KDIR],0,data->np_tot[JDIR],0,data->np_tot[IDIR],
     KOKKOS_LAMBDA (int k, int j, int i) {
         Vc(RHO,k,j,i) = rho;
+//        std::printf("rho=%e in InternalBoundary\n",Vc(RHO,k,j,i));
     });
 
 }
@@ -70,17 +65,17 @@ void InternalBoundary(Fluid<DefaultPhysics> *hydro, const real t) {
 void InternalBoundaryRad(Fluid<RadiationPhysics> *radiation, const real t) {
   IdefixArray4D<real> Vc = radiation->Vc;
   auto *data = radiation->data;
-  auto units = idfx::units;
+
   real mu = muGlob;
-  real cs = idfx::units.c/BigCGlob;
-  real T = cs*cs*mu*idfx::units.u/idfx::units.k_B;
-  real P = idfx::units.ar*std::pow(T,4.)/BigPGlob;
-  real rho = P/(cs*cs);
+  real cs = idfx::units.c/BigCGlob/idfx::units.GetVelocity();
+  real rho = idfx::units.ar/BigPGlob*std::pow(mu*idfx::units.u/idfx::units.k_B,4.)*std::pow(idfx::units.c/BigCGlob,6.)/idfx::units.GetDensity();
+  real P = rho*cs*cs;
+  real Er = BigPGlob*P;
 
   idefix_for("InternalBoundary",0,data->np_tot[KDIR],0,data->np_tot[JDIR],0,data->np_tot[IDIR],
     KOKKOS_LAMBDA (int k, int j, int i) {
-        Vc(ER,k,j,i) = units.ar*std::pow(T,4.);
-        //std::printf("Er=%e in InternalBoundary\n",Vc(ER,k,j,i));
+        Vc(ER,k,j,i) = Er;
+  //      std::printf("Er=%e in InternalBoundary\n",Vc(ER,k,j,i));
     });
 
 }
@@ -89,7 +84,7 @@ void InternalBoundaryRad(Fluid<RadiationPhysics> *radiation, const real t) {
 // Initialisation routine. Can be used to allocate
 // Arrays or variables which are used later on
 Setup::Setup(Input &input, Grid &grid, DataBlock &data, Output &output)
-{ 
+{
   gammaGlob=input.Get<real>("Hydro","gamma",0);
   muGlob=input.Get<real>("Hydro","mu",0);
   BigCGlob=input.Get<real>("Setup","BigC",0);
@@ -117,11 +112,12 @@ void Setup::InitFlow(DataBlock &data) {
     DataBlockHost d(data);
 
     real mu = muGlob;
-    real cs = idfx::units.c/BigCGlob;
-    real T = cs*cs*mu*idfx::units.u/idfx::units.k_B;
-    real P = idfx::units.ar*std::pow(T,4.)/BigPGlob;
-    real rho = P/(cs*cs);
-    std::printf("rho=%e P=%e T=%e cs=%e\n",rho,P,T,cs);
+    real cs = idfx::units.c/BigCGlob/idfx::units.GetVelocity();
+    real rho = idfx::units.ar/BigPGlob*std::pow(mu*idfx::units.u/idfx::units.k_B,4.)*std::pow(idfx::units.c/BigCGlob,6.)/idfx::units.GetDensity();
+    real P = rho*cs*cs;
+    real Er = BigPGlob*P;
+    real v = 9.480e9/idfx::units.GetVelocity();
+    std::printf("rho=%e P=%e cs=%e Er=%e v=%e\n",rho,P,cs,Er,v);
 
 
     for(int k = 0; k < d.np_tot[KDIR] ; k++) {
@@ -130,9 +126,8 @@ void Setup::InitFlow(DataBlock &data) {
 
               d.Vc(PRS,k,j,i) = P;
               d.Vc(RHO,k,j,i) = rho;
-
-              d.Vc(VX1,k,j,i) = cs;
-              d.RadVc[0](ER,k,j,i) = idfx::units.ar*std::pow(T,4.);
+              d.Vc(VX1,k,j,i) = v;
+              d.RadVc[0](ER,k,j,i) = Er;
               //d.RadVc[0](ER,k,j,i) = ZERO_F;
               d.RadVc[0](FR1,k,j,i) = ZERO_F;
             }
