9f6c381c39dcdf0af2e860f2fe468af317066341
nscepi <nicolas.scepi@gmail.com>
2026-09-09 15:54:30 +0200

    Pre-compute fractions in speed_rad

--- src/fluid/RiemannSolver/Radiationsolvers/speedRad.hpp
+++ src/fluid/RiemannSolver/Radiationsolvers/speedRad.hpp
@@ -13,7 +13,6 @@
 #include "idefix.hpp"
 #include "input.hpp"
 
-
 KOKKOS_INLINE_FUNCTION void K_SpeedsRad(real lambda[], const real *KOKKOS_RESTRICT V,
                                         int Xn, real reduced_c, real *xi) {
     real Fnorm = Kokkos::sqrt(EXPAND(V[FR1]*V[FR1] , + V[FR2]*V[FR2], + V[FR3]*V[FR3]));
@@ -24,11 +23,12 @@ KOKKOS_INLINE_FUNCTION void K_SpeedsRad(real lambda[], const real *KOKKOS_RESTRI
 
     // Eq. 85 of Melon Fuksman & Mignone 2019
     real zeta_1 = 4.-3.*f2;  // I removed an absolute value, see if it still work
-    real zeta_2 = Kokkos::sqrt(4.-3.*f2);
-    real zeta_3 = 2.*(zeta_1-zeta_2)/3.;
+    real zeta_2 = Kokkos::sqrt(zeta_1);
+    real zeta_3 = 0.6666666666666666*(zeta_1-zeta_2);
     real zeta_4 = 2.*cos_theta*cos_theta*(2.-f2-zeta_2);
     // I removed an absolute value inside the sqrt, see if it still works
     real zeta = Kokkos::sqrt(zeta_3 + zeta_4);
+
     if (f < 1.e-50) {
         lambda[1] = 0.5773502691896258; // 1/sqrt(3)
         lambda[0] = -lambda[1];
